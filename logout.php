8ec303fa6b83a005b8b55a22e526c06000852c63
gzl2004 <136467228+gzl2004@users.noreply.github.com>
2023-06-17 00:28:07 +0800

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
-setcookie("phar", "", time() - 9999999999);
-header("Location: login.php");
